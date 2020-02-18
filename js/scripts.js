1927d29a9c38d93122bcc18e7584514503137822
NutKnight-L <49262855+NutKnight-L@users.noreply.github.com>
2020-02-18 18:29:41 +0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-
+function toDou(n)
+{
+	if(n<10)
+	{
+		return '0'+n;
+	}
+	else
+	{
+		return ''+n;
+	}
+}
+window.onload=function ()
+{
+	function tick()
+	{
+		var aImg=document.getElementsByTagName('img');
+		var oDate=new Date();
+		var str=toDou(oDate.getHours())+toDou(oDate.getMinutes())+toDou(oDate.getSeconds());
+		for(var i=0; i<aImg.length; i++)
+		{
+			aImg[i].src='img/'+str[i]+'.png';
+		}
+	}
+	tick();
+	setInterval(tick, 1000);
+		
+};
